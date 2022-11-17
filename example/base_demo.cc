d2d8c57fd4ac96a38a622554c7f9770d78e38df4
Agni Biswas <agnibiswasranchi@gmail.com>
2022-11-17 11:10:06 +0100

    current progress: pre-zhang input handling
    size matching with 36 inputs is a bit bad with a monitor

--- example/base_demo.cc
+++ example/base_demo.cc
@@ -23,12 +23,26 @@ extern "C" {
 #include "Src/tagStandard52h13.h"
 }
 using namespace std;
-struct point {
+
+// ===================================================
+//  opencv aprilgrid extraction support structures
+//====================================================
+struct point
+{
   float x = 0;
   float y = 0;
+  bool operator==(const point &comparator)
+
+  {
+#ifdef DEBUGV
+    cout << "\n operator overload::" << int(this->x / 10) << "\t" << int(comparator.x / 10);
+#endif
+    return (int(this->x / RESOLUTIONMOD) == int(comparator.x / RESOLUTIONMOD) || int(this->y / RESOLUTIONMOD) == int(comparator.y / RESOLUTIONMOD));
+  }
 };
 
-struct marker_bounds {
+struct marker_bounds
+{
   /* data */
   point bound_1;
   point bound_2;
@@ -36,78 +50,149 @@ struct marker_bounds {
   point bound_4;
 
   int id = 0;
+  bool operator==(const marker_bounds &comparator)
+  {
+    return this->bound_1 == comparator.bound_1 ||
+           bound_2 == comparator.bound_2 ||
+           bound_3 == comparator.bound_3 ||
+           bound_4 == comparator.bound_4;
+  }
 };
 
-  struct acquisition_instance_Str
-  {
-    std::vector<marker_bounds> vector_acq;
-    int id;
+struct acquisition_instance_Str
+{
+  std::vector<marker_bounds> vector_acq;
+  int id;
 
-    /* data */
-  };
+  /* data */
+};
 
 using namespace cv;
-class marker_pointcloud {
+
+// ===================================================
+//  point cloud selection
+//====================================================
+class marker_pointcloud
+{
 private:
   int set_id;
   std::vector<marker_bounds> instance_vector;
 
-  const int max_iter=3;
+  const int max_iter = 3;
   std::vector<acquisition_instance_Str> vector_detection_set;
   /* data */
 public:
-
   marker_pointcloud();
   ~marker_pointcloud();
+  /// @brief
+  /// @return
+  bool motion_filter(marker_bounds input);
   void create_acquisition_instance(acquisition_instance_Str input_vector);
   void create_detect_instance(std::vector<marker_bounds> input_vector);
   int get_set_id() { return set_id; }
 };
+// ===================================================
+// fuctions in marker pointcloud class
+//====================================================
 void marker_pointcloud::create_detect_instance(
     std::vector<marker_bounds> input_vector)
 
 {
   this->instance_vector = input_vector;
-  this->set_id++;
+  marker_bounds manifold = input_vector.front(); // this is sorted by ids get id 2 bound1 and pass to motion filter
+  point ptmanifold = manifold.bound_1;
+#ifdef DEBUGV
+  cout << "input control motion filter:" << ptmanifold.x << "\t::\t" << ptmanifold.y;
+#endif
   // TODO:  you may add filters here to determine stuff currently left blank
-  acquisition_instance_Str acq;
-  acq.id= set_id;
-  acq.vector_acq=instance_vector;
-  #ifdef DEBUGV
+  bool new_image_found = this->motion_filter(manifold);
+  if (new_image_found)
+  {
+    this->set_id++;
+    acquisition_instance_Str acq;
+    acq.id = set_id;
+    acq.vector_acq = instance_vector;
+#ifdef DEBUGV
     cout << "hello";
     cout << "opset" << this->instance_vector.size();
-  #endif
-  create_acquisition_instance(acq);
+#endif
+    create_acquisition_instance(acq);
+  }
 }
-void marker_pointcloud::create_acquisition_instance(acquisition_instance_Str input_vector)
+void marker_pointcloud::create_acquisition_instance(
+    acquisition_instance_Str input_vector)
 {
-this->vector_detection_set.emplace_back(input_vector);
- 
+  this->vector_detection_set.emplace_back(input_vector);
 }
-marker_pointcloud::marker_pointcloud() {
+marker_pointcloud::marker_pointcloud()
+{
   cout << "ctor created";
   this->set_id = 0;
 }
 
 marker_pointcloud::~marker_pointcloud() {}
 
-class opencv_demo {
+// ===================================================
+//  motion detection functions
+//====================================================
+bool marker_pointcloud::motion_filter(marker_bounds input)
+{
+
+  // step 1 from the last value of the marker 2 bound 1 get values
+
+  // step 2 from the bound 1 marker 2 verify if the values are in range of the other stored values
+  //  if not in range add as a valid data set.
+
+  if (this->vector_detection_set.empty())
+  {
+    cout << "empty vector: pass through \n";
+    return (true);
+  }
+  for (std::vector<acquisition_instance_Str>::iterator
+           it = vector_detection_set.begin();
+       it != vector_detection_set.end(); ++it)
+  {
+
+    vector<marker_bounds> detectionstub = it->vector_acq;
+
+    marker_bounds mkb = detectionstub.front();
+#ifdef DEBUGV
+    cout << "\ncomapring id" << mkb.id << "\t::\t" << input.id << "\n";
+#endif
+    if (mkb == input)
+      return (false);
+  }
+  return (true);
+}
+// ===================================================
+//  opencv aprilgrid extraction
+//====================================================
+class opencv_demo
+{
 private:
   apriltag_family_t *tf = nullptr;
   apriltag_detector_t *td = nullptr;
   apriltag_detection_t *det = nullptr;
   //  this->td = apriltag_detector_create();
   marker_pointcloud detection_input;
+  int max_iter;
 
 public:
   opencv_demo(/* args */);
   ~opencv_demo();
+  opencv_demo(int max_iterations);
   Mat draw_marker(Mat input_frame, Mat Colour);
   bool set_complete = false;
+
   /// @brief generates data for the april grid point cloud
   /// @return labelled image or return a matrix of zeros if not available
 };
-Mat opencv_demo::draw_marker(Mat input_frame, Mat Colour) {
+
+// ===================================================
+//  opencv aprilgrid extraction functions
+//====================================================
+Mat opencv_demo::draw_marker(Mat input_frame, Mat Colour)
+{
   auto gray = input_frame;
   vector<marker_bounds> local_store;
   // Make an image_u8_t header for the Mat data
@@ -115,11 +200,18 @@ Mat opencv_demo::draw_marker(Mat input_frame, Mat Colour) {
                    .height = gray.rows,
                    .stride = gray.cols,
                    .buf = gray.data};
-
+#ifdef DEBUGV
   apriltag_detector_detect(td, &im);
+#endif
   zarray_t *detections = apriltag_detector_detect(td, &im);
-  if (zarray_size(detections) == APRILGRIDARRAYSIZE) {
-    for (int i = 0; i < zarray_size(detections); i++) {
+#ifdef DEBUGV
+  cout << "/\ndetected\t\t:::\t\t" << zarray_size(detections);
+#endif
+
+  if (zarray_size(detections) >= APRILGRIDARRAYSIZE)
+  {
+    for (int i = 0; i < zarray_size(detections); i++)
+    {
       apriltag_detection_t *det;
       zarray_get(detections, i, &det);
       line(Colour, Point(det->p[0][0], det->p[0][1]),
@@ -128,18 +220,19 @@ Mat opencv_demo::draw_marker(Mat input_frame, Mat Colour) {
            Point(det->p[3][0], det->p[3][1]), Scalar(0, 0, 0xff), 2);
       marker_bounds marker;
       marker.id = det->id;
-      marker.bound_1.x = det->p[1][0];
-      marker.bound_2.x = det->p[2][0];
-      marker.bound_3.x = det->p[3][0];
-      marker.bound_4.x = det->p[4][0];
-      marker.bound_1.y = det->p[1][1];
-      marker.bound_2.y = det->p[2][1];
-      marker.bound_3.y = det->p[3][1];
-      marker.bound_4.y = det->p[4][1];
+      marker.bound_1.x = det->p[0][0];
+      marker.bound_2.x = det->p[1][0];
+      marker.bound_3.x = det->p[2][0];
+      marker.bound_4.x = det->p[3][0];
+      marker.bound_1.y = det->p[0][1];
+      marker.bound_2.y = det->p[1][1];
+      marker.bound_3.y = det->p[2][1];
+      marker.bound_4.y = det->p[3][1];
       local_store.push_back(marker);
 #ifdef DEBUGV
 
-      if (det->id == 23) {
+      if (det->id == 2)
+      {
 
         cout << det->p[0][0] << "\t" << det->p[1][0] << "\t" << det->p[2][0]
              << "\t" << det->p[3][0] << "\t";
@@ -167,16 +260,18 @@ Mat opencv_demo::draw_marker(Mat input_frame, Mat Colour) {
     }
     this->detection_input.create_detect_instance(local_store);
     apriltag_detections_destroy(detections);
-   
-    int set_num = this->detection_input.get_set_id();
-    cout<<"\n"<<set_num;
-    if (set_num>= 100)
-      this->set_complete= true;
 
-   return Colour;
+    int set_num = this->detection_input.get_set_id();
+    cout << "\n"
+         << set_num;
+    if (set_num >= 10)
+      this->set_complete = true;
+
+    return Colour;
   }
 
-  else {
+  else
+  {
 #ifndef DEBUG
     return cv::Mat::zeros(Colour.size(), Colour.type());
 #else
@@ -184,23 +279,37 @@ Mat opencv_demo::draw_marker(Mat input_frame, Mat Colour) {
 #endif
   }
 }
-opencv_demo::opencv_demo(/* args */) {
+opencv_demo::opencv_demo(/* args */)
+{
+  this->max_iter = 10;
   this->tf = tag36h11_create();
   tf->width_at_border = 8;
-  tf->total_width = 10;
+  tf->total_width = max_iter;
   tf->reversed_border = false;
   this->td = apriltag_detector_create();
   apriltag_detector_add_family(td, tf);
 }
-
+opencv_demo::opencv_demo(int max_iterations)
+{
+  this->max_iter = max_iterations;
+  this->tf = tag36h11_create();
+  tf->width_at_border = 8;
+  tf->total_width = max_iter;
+  tf->reversed_border = false;
+  this->td = apriltag_detector_create();
+  apriltag_detector_add_family(td, tf);
+}
 opencv_demo::~opencv_demo() { tag36h11_destroy(tf); }
-
+// ===================================================
+// MAIN
+//====================================================
 #include <time.h>
 
 using namespace cv;
 using namespace std;
 opencv_demo aprilgrid;
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
   // Start default camera
   cv::VideoCapture cap(0);
@@ -209,7 +318,8 @@ int main(int argc, char **argv) {
   // endl;
 
   // Check if camera opened successfully
-  if (!cap.isOpened()) {
+  if (!cap.isOpened())
+  {
     cout << "Error opening video stream" << endl;
     return -1;
   }
@@ -227,7 +337,8 @@ int main(int argc, char **argv) {
   //   Size(frame_width,frame_height));
   time_t start, end;
   time(&start);
-  while (1) {
+  while (1)
+  {
 
     Mat frame, gray;
 
